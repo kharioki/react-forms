237647c1e99c7c120013eb7ea479cffaa1f2064a
kharioki <antolisah@gmail.com>
2020-08-17 06:23:20 +0300

    react form and validate

--- src/VanillaForm.jsx
+++ src/VanillaForm.jsx
@@ -1,5 +1,38 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
+function validate(values) {
+  let errors = {};
+
+  if (!values.name) {
+    errors.name = 'Required field';
+  }
+
+  return errors;
+}
 export default function VanillaForm() {
-  return <form>Form content</form>;
+  const [name, setName] = useState('');
+  const [errors, setErrors] = useState({});
+
+  useEffect(() => {
+    setErrors(validate({ name }));
+  }, [name]);
+
+  return (
+    <form>
+      <div className="input-row">
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          onChange={event => {
+            setName(event.target.value);
+          }}
+          value={name}
+        />
+      </div>
+
+      {JSON.stringify(errors)}
+    </form>
+  );
 }
