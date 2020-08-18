088ce4a0b8d0f172f59fa2abc94a22efcb881d9d
kharioki <antolisah@gmail.com>
2020-08-18 23:55:09 +0300

    using formik values, onblur and handlechange

--- src/FormikForm.jsx
+++ src/FormikForm.jsx
@@ -1,18 +1,43 @@
 import React from 'react';
+import { Formik } from 'formik';
 
 const FormikForm = () => {
   return (
-    <form>
-      <div className="input-row">
-        <label htmlFor="name">Name</label>
-        <input
-          type="text"
-          name="name"
-          id="name"
-          placeholder="Enter your name"
-        />
-      </div>
-    </form>
+    <Formik initialValues={{ name: '', email: '' }}>
+      {({ values, errors, touched, handleChange, handleBlur }) => (
+        <form>
+          <div className="input-row">
+            <label htmlFor="name">Name</label>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Enter your name"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.name}
+            />
+          </div>
+
+          <div className="input-row">
+            <label htmlFor="email">EMAIL</label>
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Enter your email"
+              onChange={handleChange}
+              onBlur={handleBlur}
+              value={values.email}
+            />
+          </div>
+
+          <div className="input-row">
+            <button type="submit">Submit</button>
+          </div>
+        </form>
+      )}
+    </Formik>
   );
 };
 
